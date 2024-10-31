f30d8a03d4854122df1ce2f950cbb9bba81c743f
Siddarth Tamse <sidtamse7@gmail.com>
2024-10-31 18:55:04 +0530

    add query for fetching the data

--- src/controllers/inventoryManagementController.js
+++ src/controllers/inventoryManagementController.js
@@ -1,69 +1,46 @@
-const { Product} = require('../models/Product'); 
-const { Category} = require('../models/Category');
-const { FinishedProduct} = require('../models/FinishedProduct');
-const { FinishedStore } = require('../models/finishedStore');
-const { PurchaseStore } = require('../models/purchasedStore');
+const sequelize = require('../db');
 
-const getFinishedGoodsData = async (req, res) => {
+ const getFinishedGoodsData = async (req, res) => {   
   try {
-    const finishedGoodsData = await FinishedProduct.findAll({
-      attributes: ['manufactured_date', 'manufactured_quantity'],
-      include: [
-        {
-          model: Product,
-          attributes: ['product_id', 'product_name'],
-          include: {
-            model: Category,
-            attributes: ['category_name'],
-          },
-        },
-        {
-          model: FinishedStore,
-          attributes: ['available_quantity'],
-        },
-      ],
-    });
-
-    const formattedData = finishedGoodsData.map((item) => ({
-      Manufacture_Date: item?.manufactured_date,
-      Product_ID: item.Product?.product_id,
-      Product_Name: item?.Product?.product_name,
-      Product_Category: item?.Product?.Category?.category_name,
-      Manufacture_Quantity: item?.manufactured_quantity,
-      Available_Quantity: item?.FinishedStore?.available_quantity,
-    }));
-
-    res.status(200).json(formattedData);
+    const query = `SELECT
+    fp.manufactured_date AS "Manufactured_Date",
+    p.product_id AS "Product_ID",
+    p.product_name AS "Product_Name",
+    c.category_name AS "Product_Category",
+    fp.manufactured_quantity AS "Manufactured_Quantity",
+    fs.available_quantity AS "Available_Quantity"
+    FROM
+    finished_products fp 
+    INNER JOIN products p ON fp.product_id = p.product_id
+    INNER JOIN categories c ON p.category_id = c.category_id 
+    LEFT JOIN finished_store fs ON fs.finished_product_id = fp.finished_product_id`;     
+    const [results] = await sequelize.query(query);     
+    res.json(results);   
   } catch (error) {
-    console.error('Error fetching finished goods data:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+         console.error('Error fetching data:', error);
+        res.status(500).json({ message: 'Error fetching product data' }); 
   }
-};
+ };
 
-const getRawMaterialInventoryData = async (req, res) => {
+ const getRawMaterialInventoryData = async (req, res) => {
   try {
-    const inventoryData = await Product.findAll({
-      attributes: ['product_id', 'product_name'],
-      include: [
-        {
-          model: Category,
-          attributes: ['category_name'],
-        },
-        {
-          model: PurchaseStore,
-          attributes: ['available_quantity'],
-        },
-      ],
-    });
-
-    const formattedData = inventoryData.map((item) => ({
-      Product_id: item?.product_id,
-      Product_Name: item?.product_name,
-      Product_Category: item?.Category?.category_name,
-      Available_Quantity: item?.PurchaseStore?.available_quantity,
-    }));
-
-    res.status(200).json(formattedData);
+    const query = `
+      SELECT 
+        p.product_id AS "Product_ID",
+        p.product_name AS "Product_Name",
+        c.category_name AS "Product_Category",
+        ps.available_quantity AS "Available_Quantity"
+      FROM 
+        products p
+      INNER JOIN 
+        categories c ON p.category_id = c.category_id
+      LEFT JOIN 
+        purchase_store ps ON ps.purchase_id = p.product_id
+`;
+ 
+    const [results] = await sequelize.query(query);
+ 
+    res.status(200).json(results);
   } catch (error) {
     console.error('Error fetching inventory data:', error);
     res.status(500).json({ message: 'Internal Server Error' });
