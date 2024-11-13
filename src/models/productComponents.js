c42362abb8493ef96e886a3f39ea1b5b0b313a08
Siddarth Tamse <sidtamse7@gmail.com>
2024-11-13 15:34:17 +0530

    add database tables for productComponent

---
+++ src/models/productComponents.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+const Product = require('../models/products');
+const Component = require('../models/component');
+
+const ProductComponents = sequelize.define('ProductComponents', {
+    product_component_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Product,
+            key: 'product_id',
+        },
+    },
+    component_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Component,
+            key: 'component_id',
+        },
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+    },
+}, { tableName: 'product_component' });
+
+module.exports = ProductComponents;
