cde7d811c8643af1e4f986fbc7f5103a4a764ec5
Siddarth Tamse <sidtamse7@gmail.com>
2025-01-27 16:09:06 +0530

    add Search Post API for items

---
+++ src/controllers/searchController.js
@@ -0,0 +1,63 @@
+const { Op } = require('sequelize');
+const Categories = require('../models/category');
+const Component = require('../models/component');
+const Product = require('../models/products');
+const FinishedProduct = require('../models/finishedProducts');
+
+exports.searchItems = async (req, res, next) => {
+    try{
+        const { searchTerm } = req.body;
+
+        if(!searchTerm){
+            return res.status(400).json({ message: 'Search term is required'});
+        }
+
+        const component = await Component.findAll({
+            where: {
+                component_name: {
+                    [Op.like]: `%${searchTerm}%`,
+                },
+            },
+        });
+
+        const products = await Product.findAll({
+            where: {
+                product_name: {
+                    [Op.like]: `%${searchTerm}%`,
+                },
+            },
+        });
+
+        const categories = await Categories.findAll({
+            where: {
+                category_name: {
+                    [Op.like]: `%${searchTerm}%`,
+                },
+            },
+        });
+
+        const finishedProducts = await FinishedProduct.findAll({
+            include: [{ model: Product, where: { product_name: { [Op.like]: `%${searchTerm}%`} } }],
+        });
+
+        if(
+            component.length === 0 &&
+            products.length === 0 &&
+            categories.length === 0 &&
+            finishedProducts.length === 0
+        ){
+            return res.status(404).json({ message: `No results found for ${searchTerm}.`});
+        }
+
+        const results = {
+            component,
+            products,
+            categories,
+            finishedProducts
+        };
+
+        return res.status(200).json(results);
+    } catch (error){
+        next(error);
+    }
+}
